ceb8be0d13609b0f04330c5902f07f01759a87e5
Perdjesk <nordesk.tasveri@gmail.com>
2013-01-06 22:51:40 +0100

    Clean of comments in MainActivity

--- mlv/src/ch/gapa/master/mlv/MainActivity.java
+++ mlv/src/ch/gapa/master/mlv/MainActivity.java
@@ -16,26 +16,11 @@ public class MainActivity extends Activity {
 
 	View selectView;
 	View currentView;
-	
-	
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// setContentView( new GraphView(this));
 
-		// Caller.getInstance ().setCache ( new MemoryCache () );
-		//
-		// StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder
-		// ().permitAll ().build ();
-		// StrictMode.setThreadPolicy ( policy );
-		// String user = "jystha";
-		// Collection<Artist> chart = User.getTopArtists ( user, API_KEY );
-		// for ( Artist artist : chart ) {
-		// Log.v ( "Artist", artist.getName () );
-		// }
-
-		// setContentView(new TouchExampleView(this));
 		selectView = new SelectView(this);
 		setCurrentView(selectView);
 	}
@@ -56,12 +41,13 @@ public class MainActivity extends Activity {
 		}
 		return super.onKeyUp(keyCode, event);
 	}
-	
+
 	public void setContentGraphView(String user, Period period) {
 		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 		imm.hideSoftInputFromWindow(
 				(findViewById(R.id.editText1)).getApplicationWindowToken(), 0);
-		ProgressDialog pd = ProgressDialog.show(this, "Graph creation", "Loading");
+		ProgressDialog pd = ProgressDialog.show(this, "Graph creation",
+				"Loading");
 		setCurrentView(new GraphView(this, user, period));
 		pd.dismiss();
 	}
